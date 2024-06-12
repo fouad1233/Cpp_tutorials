86b36c7a82f2adb5d41f0f1a88be6b45247a1987
Fouad Aladhami <fouadiadhami@gmail.com>
2024-06-12 09:41:03 +0300

    user inpu example added

---
+++ 03_cpp_user_input/user_input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x; 
+    cout << "Type a number: "; // Type a number and press enter
+    cin >> x; // Get user input from the keyboard
+    cout << "Your number is: " << x << endl; // Display the input value
+
+    //example error
+    cerr << "An example error message" << endl;
+    return 0;
+}
